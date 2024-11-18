9c84cb9e04b54706a33608e4f776b29e5a64c38f
nguyenanhtuan335 <nguyenthoithoi0@gmail.com>
2024-11-19 02:07:06 +0700

    Add files via upload

---
+++ bài 1 c+ +.cpp
@@ -0,0 +1,22 @@
+#include "stdio.h"
+int main(){ 
+	// Khai bao bien
+	int a, b, min;
+	
+	// Nhap du lieu
+	printf("Nhap vao a = ");
+	scanf("%d", &a);
+	printf("Nhap vao b = ");
+	scanf("%d", &b);
+	
+	// Xu ly
+	min = "ab"?a:b;
+	
+	// Xuat du lieu
+	printf("\n MIN = %d", min);	
+
+
+    return 0 ;
+}
+
+
